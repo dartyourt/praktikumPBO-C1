3c40dd527c952d2de41119f3e48a4ea1a0f38336
Dayys <161561501+dartyourt@users.noreply.github.com>
2025-04-28 07:49:26 +0000

    update code plimorfisme

---
+++ Praktikum7/tugas/MAnabul.java
@@ -0,0 +1,13 @@
+public class MAnabul {
+    public static void main(String[] args) {
+        Anabul anabul1 = new Anjing("Doggo");
+        Anabul anabul2 = new Kucing("Kitty");
+        Anabul anabul3 = new Burung("Tweety");
+        Kucing K = new Kucing("Ayana");
+
+        anabul1.printInfo();
+        anabul2.printInfo();
+        anabul3.printInfo();
+        K.printInfo();
+    }
+}
