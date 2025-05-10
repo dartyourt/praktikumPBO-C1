ddf38ad635b8d30977422e8f0877d35a13a95e0e
Dayys <161561501+dartyourt@users.noreply.github.com>
2025-05-10 20:31:37 +0700

    update code

--- Praktikum9/praktikum/koleksi/koleksi.java
+++ Praktikum9/praktikum/koleksi/koleksi.java
@@ -16,4 +16,57 @@ public class koleksi <T> {
     public int getSize() {
         return nbElm;
     }
+
+    public T getIsi(int index) {
+        if (index >= 0 && index < nbElm) {
+            return wadah[index];
+        } else {
+            return null;
+        }
+    }
+
+    public void setIsi(int index, T isi) {
+        if (index >= 0 && index < nbElm) {
+            wadah[index] = isi;
+        }
+    }
+
+    public void add(T isi) {
+        if (nbElm < wadah.length) {
+            wadah[nbElm] = isi;
+            nbElm++;
+        } else {
+            System.out.println("Koleksi sudah penuh");
+        }
+    }
+
+    public T delete() {
+        //kamus lokal
+        T isi;
+        //algoritma
+        if (nbElm > 0) {
+            nbElm--;
+            isi = wadah[nbElm];
+            wadah[nbElm] = null;
+            return isi;
+        } else {
+            System.out.println("Koleksi sudah kosong");
+            return null;
+        }
+    }
+
+    public void showAll(){
+        //kamus lokal
+        int i;
+        //algoritma
+        if (nbElm > 0) {
+            for (i = 0; i < nbElm; i++) {
+                System.out.println(getIsi(i) + " ");
+            }
+        } else {
+            System.out.println("Koleksi sudah kosong");
+        }   
+    }
+
+
 }
